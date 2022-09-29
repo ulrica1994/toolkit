7beb7a2a4bb3ba4e42979671f096b800c17ac4c6
fujianguo <fujianguo@hayzone.com>
2022-09-29 13:14:40 +0800

    update tools

--- tools.go
+++ tools.go
@@ -12,53 +12,54 @@ import (
 )
 
 const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
 // Tools is the type used to instantiate this module.Any variable of this type will have ccess
 // to all the methods with the reciver *Tools
 type Tools struct {
-	MaxFileSize		int
-	allowedTypes	[]string
+	MaxFileSize  int
+	allowedTypes []string
 }
 
 func (t *Tools) RandomString(n int) string {
-	s, r := make([]rune,n),[]rune(randomStringSource)
+	s, r := make([]rune, n), []rune(randomStringSource)
 	for i := range s {
-		p,_ := rand.Prime(rand.Reader,len(r))
-		x, y := p.Uint64(),uint64(len(r))
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
 		s[i] = r[x%y]
 	}
 	return string(s)
 }
 
 type UploadFile struct {
-	NewFileName			string
-	OriginalFileName	string
-	FileSize			string
+	NewFileName      string
+	OriginalFileName string
+	FileSize         int64
 }
 
-func (t *Tools) UploadFile(r *http.Request,uploadDir string, rename ...bool) ([]*UploadFile,error) {
+func (t *Tools) UploadFile(r *http.Request, uploadDir string, rename ...bool) ([]*UploadFile, error) {
 	renameFile := true
 	if len(rename) > 0 {
 		renameFile = rename[0]
-	}[
-	var uploadFiles = []*UploadFile
+	}
+	var uploadFiles []*UploadFile
 	if t.MaxFileSize == 0 {
 		t.MaxFileSize = 1024 * 1024 * 1024
-	}	
+	}
 	err := r.ParseMultipartForm(int64(t.MaxFileSize))
 	if err != nil {
-		return nil,errors.New("the upload file  is too big")
+		return nil, errors.New("the upload file  is too big")
 	}
 	for _, fHeadeers := range r.MultipartForm.File {
 		for _, hdr := range fHeadeers {
-			uploadFiles, err = func(uploadFiles []*UploadFile) ([]*UploadFile, error){
+			uploadFiles, err = func(uploadFiles []*UploadFile) ([]*UploadFile, error) {
 				var uploadFile UploadFile
 				infile, err := hdr.Open()
 				if err != nil {
-					return nil,err
+					return nil, err
 				}
 				defer infile.Close()
 
-				buff := make([]byte,512)
+				buff := make([]byte, 512)
 				_, err = infile.Read(buff)
 				if err != nil {
 					return nil, err
@@ -69,7 +70,7 @@ func (t *Tools) UploadFile(r *http.Request,uploadDir string, rename ...bool) ([]
 				// allowedTypes := []string("image/jpeg","image/png","image/gif")
 				if len(t.allowedTypes) > 0 {
 					for _, x := range t.allowedTypes {
-						if strings.EqualFold(fileType,x) {
+						if strings.EqualFold(fileType, x) {
 							allowed = true
 						}
 					}
@@ -78,28 +79,28 @@ func (t *Tools) UploadFile(r *http.Request,uploadDir string, rename ...bool) ([]
 					allowed = true
 				}
 				if !allowed {
-					return nil,errors.New("the upload file type is not permitted")
+					return nil, errors.New("the upload file type is not permitted")
 				}
-				_, err = infile.Seek(0,0)
+				_, err = infile.Seek(0, 0)
 				if err != nil {
-					return nil,err
+					return nil, err
 				}
 				if renameFile {
-					uploadFile.NewFileName = fmt.Sprintf("%s%s",t.RandomString(25),filepath.Ext(hdr.Filename))
+					uploadFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25), filepath.Ext(hdr.Filename))
 				} else {
 					uploadFile.NewFileName = hdr.Filename
 				}
 				var outfile *os.File
 				defer outfile.Close()
-				if outfile, err = os.Create(filepath.Join(uploadDir,uploadFile.NewFileName)); err != nil {
-					return nil, err 
+				if outfile, err = os.Create(filepath.Join(uploadDir, uploadFile.NewFileName)); err != nil {
+					return nil, err
 				} else {
-					fileSize, err := io.Copy(outfile,infile)
+					fileSize, err := io.Copy(outfile, infile)
 					if err != nil {
 						return nil, err
 					}
 					uploadFile.FileSize = fileSize
-					uploadFiles = append(uploadFiles, uploadFile)
+					uploadFiles = append(uploadFiles, &uploadFile)
 					return uploadFiles, nil
 				}
 			}(uploadFiles)
@@ -107,6 +108,6 @@ func (t *Tools) UploadFile(r *http.Request,uploadDir string, rename ...bool) ([]
 				return uploadFiles, err
 			}
 		}
-		return uploadFiles, nil
 	}
+	return uploadFiles, nil
 }
